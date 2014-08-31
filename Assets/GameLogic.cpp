a953981bc724f0d3d0fe9d361fc800bea8361afd
Jason Howard <jasonh78@gmail.com>
2014-08-31 15:32:11 -0700

    Implemented function to draw X in window.

--- Assets/GameLogic.cpp
+++ Assets/GameLogic.cpp
@@ -10,6 +10,20 @@ GameLogic::GameLogic( D3DGraphics* gfx )
 	m_xplayer			= m_xPlayerInstance.GetPlayerXInstance();
 }
 
+GameLogic::~GameLogic()
+{
+	if ( m_pGfx )
+	{
+		m_pGfx->~D3DGraphics();
+		m_pGfx = NULL;
+	}
+	if ( m_xplayer )
+	{
+		m_xplayer = NULL;
+	}
+
+}
+
 void GameLogic::m_DEBUG_DrawPlayerPieceCircle( )
 {
 	m_pGfx->DrawPlayerPieceCircle( 500, 300, 255, 255, 255 );
@@ -18,6 +32,7 @@ void GameLogic::m_DEBUG_DrawPlayerPieceCircle( )
 void GameLogic::m_DEBUG_DrawPlayerPIeceX()
 {
 	// call gfx function to draw x
+	m_pGfx->DrawPlayerPieceX( 260,340, 255,255,255 );
 }
 
 void GameLogic::DrawGameBoard()
