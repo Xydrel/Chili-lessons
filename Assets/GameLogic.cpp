01eec2cbcd8bf105f5a15bdb77b8acfd366be6c8
Jason Howard <jasonh78@gmail.com>
2014-09-07 14:20:18 -0700

    Modification to x and y math for drawing piece on the gameboard.

--- Assets/GameLogic.cpp
+++ Assets/GameLogic.cpp
@@ -50,8 +50,8 @@ void GameLogic::DrawPlayerPieceX( float x, float y, int r, int g, int b )
 {
 	if ( m_pGfx )
 	{
-		m_pGfx->DrawLine( x, y, 110, 190, r, g, b );
-		m_pGfx->DrawLine( x, y, 190, 110, r, g, b );
+		m_pGfx->DrawLine( x + 15, y + 15, x+ 84, y + 84, r, g, b );
+		m_pGfx->DrawLine( x + 15, y + 84, x+ 84, y + 15, r, g, b );
 	}
 
 	// there will actually have to be game logic to determine which row
@@ -63,21 +63,22 @@ void GameLogic::DrawPlayerPieceCircle( float x, float y, int r, int g, int b )
 {
 	if ( m_pGfx )
 	{
-		m_pGfx->DrawCircle( x, y, 40, r, g, b );
+		m_pGfx->DrawCircle( x, y, 35, r, g, b );
 	}
 }
 
 // --------------------------------------------------------------------------------
-void GameLogic::DrawX( int x, int y )
+void GameLogic::DrawX( int x, int y)
 {
 	// Draw the X game piece
-	DrawPlayerPieceX( x, y, 255, 255, 255 );
+	DrawPlayerPieceX( x, y, 255, 0, 0 );
 }
 
 // --------------------------------------------------------------------------------
 void GameLogic::DrawO( int x, int y )
 {
 	// Draw the O game piece
+	DrawPlayerPieceCircle( x + 49, y + 49, 0, 0, 255 );
 }
 
 // --------------------------------------------------------------------------------
@@ -85,14 +86,14 @@ void GameLogic::DrawGrid(int x, int y)
 {
 	if ( m_pGfx )
 	{
-		m_pGfx->DrawLine( 0 + x, 299 + x, 0 + y, 0 + y, 255, 255, 255 );		// draw top line
-		m_pGfx->DrawLine( 0 + x, 299 + x, 299 + y, 299 + y, 255, 255, 255 );	// draw bottom
-		m_pGfx->DrawLine( 0 + x, 299 + x, 199 + y, 199 + y, 255, 255, 255 );	// draw lower cross line
-		m_pGfx->DrawLine( 0 + x, 299 + x, 99 + y, 99 + y, 255, 255, 255 );		// draw top cross line
-		m_pGfx->DrawLine( 0 + x, 0 + x, 0 + y, 299 + y, 255, 255, 255 );		// draw left side
-		m_pGfx->DrawLine( 299 + x, 299 + x, 0 + y, 299 + y, 255, 255, 255 );	// draw right side
-		m_pGfx->DrawLine( 99 + x, 99 + x, 0 + y, 299 + y, 255, 255, 255 );		// draw left inside side
-		m_pGfx->DrawLine( 199 + x, 199 + x, 0 + y, 299 + y, 255, 255, 255 );	// draw right inside side
+		m_pGfx->DrawLine( 0 + x,	0 + y,	 299 + x, 0 + y,   255, 255, 255 );		// draw top line
+		m_pGfx->DrawLine( 0 + x,	299 + y, 299 + x, 299 + y, 255, 255, 255 );	// draw bottom
+		m_pGfx->DrawLine( 0 + x,	199 + y, 299 + x, 199 + y, 255, 255, 255 );	// draw lower cross line
+		m_pGfx->DrawLine( 0 + x,	99 + y,	 299 + x, 99 + y,  255, 255, 255 );		// draw top cross line
+		m_pGfx->DrawLine( 0 + x,	0 + y,	 0 + x,	  299 + y, 255, 255, 255 );		// draw left side
+		m_pGfx->DrawLine( 299 + x,	0 + y,	 299 + x, 299 + y, 255, 255, 255 );	// draw right side
+		m_pGfx->DrawLine( 99 + x,	0 + y,	 99 + x,  299 + y, 255, 255, 255 );		// draw left inside side
+		m_pGfx->DrawLine( 199 + x,	0 + y,	 199 + x, 299 + y, 255, 255, 255 );	// draw right inside side
 	}
 }
 
