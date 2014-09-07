ab299e1b9be789d89c50216403ffd7570bcb250f
Jason Howard <jasonh78@gmail.com>
2014-09-07 14:19:42 -0700

    Creation of Cell state setting for gameplay logic to determin the play state of each cell.

--- Assets/GameBoard.cpp
+++ Assets/GameBoard.cpp
@@ -3,10 +3,60 @@
 
 GameBoard::GameBoard()
 {
-
+	for ( int index = 0; index < 9; index++ )
+	{
+		SetCellState( index, EMPTY );
+	}
 }
 
 GameBoard::~GameBoard()
 {
 
+}
+
+void GameBoard::SetCellState(int index, CellState state)
+{
+	switch ( index )
+	{
+		case 0: s0 = state; break;
+		case 1: s1 = state; break;
+		case 2: s2 = state; break;
+		case 3: s3 = state; break;
+		case 4: s4 = state; break;
+		case 5: s5 = state; break;
+		case 6: s6 = state; break;
+		case 7: s7 = state; break;
+		case 8: s8 = state; break;
+		default: break;
+	}
+}
+
+void GameBoard::SetGameBoardCords()
+{
+	// set the m_gameBoardArray[3][3] values
+	// Creating first Vertical row
+
+	// NOT WORKING YET
+
+	//std::vector<int> rowYcenter = { 49, 149, 249 };
+	//for ( int i = 0; i <= rowYcenter.size( ); ++i )
+	//{
+	//	int y = rowYcenter[i];
+	//	std::vector<int, int> tempVect = { 49, y };
+	//	m_rowOneVect.push_back( tempVect );
+	//}
+
+	//for ( int i = 0; i <= rowYcenter.size( ); ++i )
+	//{
+	//	int y = rowYcenter[i];
+	//	std::vector<int, int> tempVect = { 149, y };
+	//	m_rowTwoVect.push_back( tempVect );
+	//}
+
+	//for ( int i = 0; i <= rowYcenter.size( ); ++i )
+	//{
+	//	int y = rowYcenter[i];
+	//	std::vector<int, int> tempVect = { 249, y };
+	//	m_rowThreeVect.push_back( tempVect );
+	//}
 }
--- Assets/GameBoard.h
+++ Assets/GameBoard.h
@@ -1,5 +1,12 @@
+/*
+* Class Stores the values for the game board array and
+* cords for the center of each game block where a piece
+* can be drawn.
+*/
+#include "Cordenant.h"
+#include <vector>
 
-
+//using namespace std;
 
 class GameBoard
 {
@@ -8,6 +15,32 @@ public:
 	~GameBoard();
 
 private:
+
+	//std::vector<std::vector<int, int>>		m_gameBoardVect;	// The main gameboard vector
+	//std::vector<int, int>					m_rowOneVect;		// The first Vertical row of the gameboard
+	//std::vector<int, int>					m_rowTwoVect;		// Second Vertical Row of the gameboard
+	//std::vector<int, int>					m_rowThreeVect;		// Third Vertical row of the gameboard
+
+	enum CellState
+	{
+		EMPTY,
+		X,
+		O,
+	};
+
+	CellState s0;
+	CellState s1;
+	CellState s2;
+	CellState s3;
+	CellState s4;
+	CellState s5;
+	CellState s6;
+	CellState s7;
+	CellState s8;
+
+public: 
+	void SetCellState(int index, CellState state);
+	void SetGameBoardCords();
 
 };
 
