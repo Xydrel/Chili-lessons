96a881d6ac8f480f1d1f512cd911a5253e34ea28
Jason.howard <jasonh78@gmail.com>
2014-09-19 13:11:34 -0700

    Definitions of AI Gameplay functions

--- Assets/Behaviour/AI.h
+++ Assets/Behaviour/AI.h
@@ -2,6 +2,28 @@
 
 class AI
 {
-	AI() {}
-	~AI() {}
+// ================================================
+// Public Member Constructor
+public:
+	AI( GameBoard gameBoard ) { m_gameBoard = gameBoard; }
+	~AI() { if (m_gameBoard) {delete m_gameBoard; m_gameBoard = NULL;} }
+
+	void AIMoveX();
+	void AIMoveO();
+
+// ================================================
+// Protected Member Functions
+protected:
+
+
+// ================================================
+// Private Member Functions
+private:
+	void aiPickLocationOnGameBoard();
+
+// ================================================
+// Private Member Variables
+private:
+	GameBoard* m_gameBoard;
+
 };
