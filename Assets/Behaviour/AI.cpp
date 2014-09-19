3e4a28d7aaf06e6313696ab4c24b1e4f1bb7529c
Jason Howard <jason.howard@gazillion.com>
2014-09-19 16:18:57 -0700

    Commnting till Error is resolved

--- Assets/Behaviour/AI.cpp
+++ Assets/Behaviour/AI.cpp
@@ -1,23 +1,23 @@
-#include "Behaviour/AI.h"
-
-
-void AI::AIMoveX()
-{
-
-}
-
-void AI::AIMoveO()
-{
-
-}
-
-void AI::aiPickLocationOnGameBoard()
-{
-	/*
-	* The implementation from the video will be that the computer
-	* will pick a random EMPTY location and place a piece there
-	* no matter where the human player at the computer decides to 
-	* do. I will extend the logic here to make the game AI more 
-	* intelligent.
-	*/
-}
+#include "AI.h"
+//
+//
+//void AI::AIMoveX()
+//{
+//
+//}
+//
+//void AI::AIMoveO()
+//{
+//
+//}
+//
+//void AI::aiPickLocationOnGameBoard()
+//{
+//	/*
+//	* The implementation from the video will be that the computer
+//	* will pick a random EMPTY location and place a piece there
+//	* no matter where the human player at the computer decides to 
+//	* do. I will extend the logic here to make the game AI more 
+//	* intelligent.
+//	*/
+//}
