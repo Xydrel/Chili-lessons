1d11be4898cf72cd52ccab1ac4c2313b51082ee3
Jason Howard <jason.howard@gazillion.com>
2014-09-19 16:19:10 -0700

    Commenting till error is resolved

--- Assets/Behaviour/AI.h
+++ Assets/Behaviour/AI.h
@@ -1,29 +1,28 @@
-
-
-class AI
-{
-// ================================================
-// Public Member Constructor
-public:
-	AI( GameBoard gameBoard ) { m_gameBoard = gameBoard; }
-	~AI() { if (m_gameBoard) {delete m_gameBoard; m_gameBoard = NULL;} }
-
-	void AIMoveX();
-	void AIMoveO();
-
-// ================================================
-// Protected Member Functions
-protected:
-
-
-// ================================================
-// Private Member Functions
-private:
-	void aiPickLocationOnGameBoard();
-
-// ================================================
-// Private Member Variables
-private:
-	GameBoard* m_gameBoard;
-
-};
+//
+//class AI
+//{
+//// ================================================
+//// Public Member Constructor
+//public:
+//	AI( GameBoard* gameBoard ) { m_pGameBoard = gameBoard; }
+//	~AI() { if (m_pGameBoard) { delete m_pGameBoard; m_pGameBoard = NULL; } }
+//
+//	void AIMoveX();
+//	void AIMoveO();
+//
+//// ================================================
+//// Protected Member Functions
+//protected:
+//
+//
+//// ================================================
+//// Private Member Functions
+//private:
+//	void aiPickLocationOnGameBoard();
+//
+//// ================================================
+//// Private Member Variables
+//private:
+//	GameBoard* m_pGameBoard;
+//
+//};
