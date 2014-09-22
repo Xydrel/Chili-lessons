8a02a681a2066c8f0da7375e04e8abc8b172df6a
Jason Howard <jasonh78@gmail.com>
2014-09-21 22:11:49 -0700

    Code to perform AI selection of cell to place piece. WIP

--- Assets/Behaviour/AI.cpp
+++ Assets/Behaviour/AI.cpp
@@ -1,23 +1,67 @@
 #include "AI.h"
-//
-//
-//void AI::AIMoveX()
-//{
-//
-//}
-//
-//void AI::AIMoveO()
-//{
-//
-//}
-//
-//void AI::aiPickLocationOnGameBoard()
-//{
-//	/*
-//	* The implementation from the video will be that the computer
-//	* will pick a random EMPTY location and place a piece there
-//	* no matter where the human player at the computer decides to 
-//	* do. I will extend the logic here to make the game AI more 
-//	* intelligent.
-//	*/
-//}
+#include <algorithm>
+#include <ctime>
+
+
+
+GameBoard::CellState AI::AIMoveX( )
+{
+	GameBoard::CellState playerPiece = GameBoard::CellState::X;
+	return playerPiece;
+}
+
+GameBoard::CellState AI::AIMoveO( )
+{
+	GameBoard::CellState playerPiece = GameBoard::CellState::O;
+	return playerPiece;
+}
+
+int AI::RandomSelection(std::vector<int> numArray )
+{
+	int count = sizeof( numArray );
+	int randomIndex = rand() % count;
+	return randomIndex;
+}
+
+void AI::AiPickLocationOnGameBoard()
+{
+	/*
+	* The implementation from the video will be that the computer
+	* will pick a random EMPTY location and place a piece there
+	* no matter where the human player at the computer decides to 
+	* do. I will extend the logic here to make the game AI more 
+	* intelligent.
+	*/
+
+	int index;
+
+	if ( m_pGameBoard->GetCellState( 0 ) == GameBoard::CellState::EMPTY ||
+		 m_pGameBoard->GetCellState( 2 ) == GameBoard::CellState::EMPTY ||
+		 m_pGameBoard->GetCellState( 6 ) == GameBoard::CellState::EMPTY ||
+		 m_pGameBoard->GetCellState( 8 ) == GameBoard::CellState::EMPTY ||
+		 m_pGameBoard->GetCellState( 4 ) == GameBoard::CellState::EMPTY )
+	{
+		// place a piece in one of these places
+		std::vector<int> charNumArray = { 0, 2, 6, 8, 4 };
+		index = RandomSelection( charNumArray );
+
+		m_pGameBoard->SetCellState( charNumArray[index], AIMoveO() );
+	}
+	else if ( m_pGameBoard->GetCellState( 4 ) == m_pGameBoard->EMPTY )
+	{
+		// select one of the above locations to place a piece
+		std::vector<int> charNumArray = { 1, 3, 5, 7 };
+		index = RandomSelection( charNumArray );
+
+		m_pGameBoard->SetCellState( charNumArray[index], AIMoveO() );
+	}
+	else
+	{
+		// evaluate the board for potential winning locations
+		std::vector<int> charNumArray = { 1, 3, 5, 7 };
+		index = RandomSelection( charNumArray );
+
+		m_pGameBoard->SetCellState( charNumArray[index], AIMoveO() );
+	}
+
+}
--- Assets/Behaviour/AI.h
+++ Assets/Behaviour/AI.h
@@ -1,28 +1,40 @@
-//
-//class AI
-//{
-//// ================================================
-//// Public Member Constructor
-//public:
-//	AI( GameBoard* gameBoard ) { m_pGameBoard = gameBoard; }
-//	~AI() { if (m_pGameBoard) { delete m_pGameBoard; m_pGameBoard = NULL; } }
-//
-//	void AIMoveX();
-//	void AIMoveO();
-//
-//// ================================================
-//// Protected Member Functions
-//protected:
-//
-//
-//// ================================================
-//// Private Member Functions
-//private:
-//	void aiPickLocationOnGameBoard();
-//
-//// ================================================
-//// Private Member Variables
-//private:
-//	GameBoard* m_pGameBoard;
-//
-//};
+#pragma once
+
+#include "../GameBoard.h"
+
+#include <vector>
+
+
+class AI
+{
+// ================================================
+// Public Member Constructor
+public:
+	AI( GameBoard* gameBoard ) { m_pGameBoard = gameBoard; }
+	~AI() { if (m_pGameBoard) { delete m_pGameBoard; m_pGameBoard = NULL; } }
+
+	GameBoard::CellState AIMoveX( );
+	GameBoard::CellState AIMoveO( );
+
+	int RandomSelection( std::vector<int> charArray );
+
+public:
+	GameBoard::CellState AIPlayer = m_pGameBoard->playerB;
+	void AiPickLocationOnGameBoard();
+
+// ================================================
+// Protected Member Functions
+protected:
+	GameBoard::CellState m_playPiece;
+
+
+// ================================================
+// Private Member Functions
+private:
+
+// ================================================
+// Private Member Variables
+private:
+	GameBoard* m_pGameBoard;
+
+};
