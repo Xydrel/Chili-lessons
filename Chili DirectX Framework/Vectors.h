57316c64e579b8a3f9cb3cfba3f19e2c2f260a1e
Jason Howard <jasonh78@gmail.com>
2015-11-03 11:56:48 -0800

    - Vector arithmetic operations implementation

--- Chili DirectX Framework/Vectors.h
+++ Chili DirectX Framework/Vectors.h
@@ -2,68 +2,101 @@
 
 #include "cmath"
 
-typedef long double LONGD64;
-typedef signed short SHORT16;
 
-
-class Vector2
+template <typename T>
+struct Vector2
 {
 public:
 	// default override
 	Vector2() : x(0), y(0) {}
 
-	Vector2(LONGD64 x, LONGD64 y)
+	Vector2(T x, T y)
 		: x(x)
 		, y(y) 
 	{
-		magnitude = sqrt(pow(x, 2) + pow(y, 2));
+		magnitude = ComputeMag();
 	}
 
-	Vector2(LONGD64 mag, SHORT16 theta) 
+	Vector2(T mag, T theta)
 		: magnitude(mag)
 		, theta(theta)
 	{
-		x = (magnitude*(sin(theta)));
-		y = (magnitude*(cos(theta)));
+		x = ComputeX(magnitude, theta);
+		y = ComputeY(magnitude, theta);
 	}
 
 	~Vector2();
 
+	/* Compute self magnitude */
+	T ComputeMag() { return sqrt(x*x + y*y); }
+
+	/* Compute self x component */
+	T ComputeX(T mag, T theta) { return mag*(sin(theta)); }
+
+	/* Compute self y component */
+	T ComputeY(T mag, T theta) { return mag*(cos(theta)); }
+
+	/* Scale a vector */
+	void SelfScale(T scalar) { x = scalar*x, y = scalar*y; }
+	void Scale(T scalar, Vector3& vec) { vec.x = scalar*vec.x, vec.y = scalar*vec.y; }
+
 private:
-	LONGD64 pi = (2 * acos(0.0));
-	LONGD64 x, y, magnitude;
-	SHORT16 theta;
+	unsigned double pi = (2 * acos(0.0));
+	T x, y, magnitude;
+	T theta;
 
 };
 
-class Vector3
+template <typename T>
+struct Vector3
 {
 public:
 	// Default override
 	Vector3() : x(0), y(0), z(0) {}
 
-	Vector3(LONGD64 x, LONGD64 y, LONGD64 z)
+	Vector3(T x, T y, T z)
 		: x(x)
 		, y(y)
 		, z(z) 
 	{
-		magnitude = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+		magnitude = ComputeMag();
 	}
 
-	Vector3(LONGD64 mag, SHORT16 theta) 
+	Vector3(T mag, T theta)
 		: magnitude(mag)
 		, theta(theta) 
 	{
-		x = (magnitude*(sin(theta)));
-		y = (magnitude*(cos(theta)));
-		z = (magnitude*(tan(theta)));
+		x = ComputeX(magnitude, theta);
+		y = ComputeY(magnitude, theta);
+		z = ComputeZ(magnitude, theta);
 	}
 
 	~Vector3();
 
+	/* Compute self magnitude */
+	T ComputeMag() { return sqrt(x*x + y*y + z*z); }
+
+	/* Compute self x component */
+	T ComputeX(T mag, T theta) { return mag*(sin(theta)); }
+
+	/* Compute self y component */
+	T ComputeY(T mag, T theta) { return mag*(cos(theta)); }
+
+	/* Compute self z component */
+	T ComputeZ(T mag, T theta) { return mag*(tan(theta)); }
+
+	/* Scale a vector */
+	void SelfScale(T scalar) { x = scalar*x, y = scalar*y, z = scalar*z; }
+	void Scale(T scalar, Vector3& vec) { vec.x = scalar*vec.x, vec.y = scalar*vec.y, vec.z = scalar*vec.z; }
+
+	/* Compute vector cross product */
+
+	/* Compute vector dot product */
+
+
 private:
-	LONGD64 pi = (2 * acos(0.0));
-	LONGD64 x, y, z, magnitude;
-	SHORT16 theta;
+	unsigned double pi = (2 * acos(0.0));
+	T x, y, z, magnitude;
+	T theta;
 
 };
